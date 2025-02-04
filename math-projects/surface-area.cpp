fe3133062fa527f5a38b30e01b89d63603aa6d0b
Marshad-mars <marshadchoudhury3@gmail.com>
2025-02-04 19:41:57 +0530

    Find the total surface area of cube or cuboid

---
+++ math-projects/surface-area.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    std::cout << "Find the surface area of any 3-D figure~" << "\n" << "   " << endl;
+
+    cout << "Choose any one of the following 3-D figure-" << endl;
+    cout << "1. Cube" << "\n" << "2. Cuboid" << "\n" << "3. Cone" << "\n" << "4. Sphere" << endl; 
+    int 
+    return 0;
+}
