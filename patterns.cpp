579537b9388f86a0a66602ae8b5ea307a75283b8
Marshad-mars <marshadchoudhury3@gmail.com>
2025-02-20 18:39:23 +0530

    Added a new project of patterns

--- patterns.cpp
+++ patterns.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(){
     cout << "               {Find your desired pattern!}" << endl;
-    char chos[2];
-    int n, i, j, val=0;
+    char chos[10];
+    int n, i, j, val=0;   //extra variable- val;
 
     cout << " " << "\n" << "Options- Square pattern(sp), Rectangular pattern(rp), Triangular pattern(tp)." << endl;
     cout << "Enter your choise: ";
@@ -21,9 +21,30 @@ int main(){
         }
     }
     else if(chos == "rp"){
+        int len;
         cout << "Rectangular Pattern" << endl;
         cout << "Enter the range(n): ";
         cin >> n;
+        cout << "Enter the length range of your rectangular pattern: ";
+        cin >> len;
+        for(i=0;i<=n;i++){
+            for(j=0;j<=len;j++){
+                cout << "* ";
+            }
+            cout << endl;
+        }
+    }
+    else if(chos == "tp"){
+        cout << "Triangular Pattern" << endl;
+        cout << "Enter the range(n): ";
+        cin >> n;
+        for(i=1;i<n;i++){
+            val += i;
+            for(j=0;j<=val;j++){
+                cout << "* ";
+            }
+            cout << endl;
+        }
     }
     return 0;
 }
