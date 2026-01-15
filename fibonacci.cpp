972f2a58a3fdc60a4f2e8fa2bc67f940362acb41
Marshad-mars <marshadchoudhury3@gmail.com>
2026-01-15 16:21:16 +0530

    new idea for finding the fibonacci of a value

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -22,3 +22,27 @@ int main(){
     cout << "The nth fibonacci would be: " << fib(n) << endl;
     return 0;
 }
+//     There is also a different method of finding the fibonacci of a certain value. Its by the "incrimentation of previous,most previous and current value"
+// int fib(int n){
+//     int most_previous, previous, current;
+//     for(int i = 0; i+1 <= n; i++){
+//         if(i < 2){
+//             most_previous = 0;
+//             if(i == 1) previous++;
+//             current = most_previous + previous;
+//         }
+//         else{
+//             most_previous = previous;
+//             previous = current;
+//             current = most_previous + previous;
+//         }
+//     }
+//     return current;
+// }
+// int main(){
+//     int range;
+//     cout << "Enter the number: ";
+//     cin >> range;
+//     cout << "This is the fibonacii to the given number range: " << fib(range) << endl;
+//     return 0;
+// }
